399e99487ea681e6b0f3143b7466e5d564ab726d
Glitch (glitch-hello-website) <none>
2022-12-07 12:23:52 +0000

    💖🏮 Checkpoint ./client/mship/login.html:27621106/991 ./client/mship/register.js:45336988/1106

--- client/mship/register.js
+++ client/mship/register.js
@@ -1,49 +1,13 @@
+//registration is working again!!
 
-while (true) {
-alert("Registration is disabled!")
-}
-
-// Author : https://themeforest.net/user/pixinvent
-const testarray = [
-  "James bond",
-  "Tom and jerry",
-  "Mayor of ur mom"
-]
-
-var DB = {
-  "Log1":{
-    "name": "Felix",
-    "role": "developer",
-    "lvl": "40"
+var user = {
+  user1: {
+    name: "Attacker",
+    level: "0",
+    isVIP: true,
+    password: 1234
   },
-  "Log2":{
-    "name": "Js",
-    "role": "developer",
-    "lvl": "30"
+  user2:{
+    name: "cws",
   }
 }
-
-let toadd = {} //hi there!
-
-
-// ChatBox
-// Cws: 
-// Felix: Hello!
-console.log(testarray)
-console.log(DB)
-console.log("touch grass");
-function register(){ //hello there attackre, nice to meet you here
-  console.log("yes")
-  testarray.push("Liber")
-  console.log(testarray)
-}
-
-
-
-
-
-//Do not touch, or mess up pls 
-/*19/11/2022 - (test) appending array in a button function
-  this proves that appending object automatically is future available :>
-*/
-
